98d90753ead2230c8709ddcf790b4ea69c4bfc62
Matthew Logan Shaw <mshaw11@qub.ac.uk>
2019-10-22 22:49:37 +0100

    Recorded linear accel steps. Will continue with excel data.

---
+++ Assets/Scripts/Accelerometer/LinearSample.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearSample : MonoBehaviour
+{
+    public float x, y, z;
+    public LinearSample(float x, float y, float z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public string ToCsv()
+    {
+        return x.ToString() + ", " + y.ToString() + ", " + z.ToString() + "\n";
+    }
+}
---
+++ Assets/Scripts/Scenes/LinearRecorder.cs
@@ -0,0 +1,30 @@
+﻿using Assets.Scripts;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Assets.Scripts
+{
+    public class LinearRecorder : MonoBehaviour
+    {
+
+        PersistentSave fileSystem = new PersistentSave();
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            fileSystem.OpenFileToWrite(Application.persistentDataPath + DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss") + ".txt");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            fileSystem.WriteToFile(ToCsv(Input.acceleration.x, Input.acceleration.y, Input.acceleration.z));
+        }
+
+        public string ToCsv(float x, float y , float z)
+        {
+            return x.ToString() + ", " + y.ToString() + ", " + z.ToString() + "\n";
+        }
+    }
+}
